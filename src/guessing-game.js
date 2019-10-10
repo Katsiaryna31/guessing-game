9627db847563419079bbda80d681db3c5cf6f3fe
Katsiaryna31 <katia310790@mail.ru>
2019-10-10 21:59:17 +0300

    feat: make guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
 
+    }
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
     guess() {
-
+      this.guessNumber = Math.ceil((this.max + this.min)/2);
+      return this.guessNumber;
     }
-
     lower() {
-
+      this.setRange(this.min, this.guessNumber);
     }
 
     greater() {
-
+      this.setRange(this.guessNumber, this.max);
     }
+
 }
 
 module.exports = GuessingGame;
